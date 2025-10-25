37b94316532ba7c243d88d6ba589a385170b9079
Aadit Mandhania <aadit.mandhania.coding@gmail.com>
2025-10-25 13:31:49 -0500

    Add file saving: log daily progress to progress.txt

--- v1/leetcode_tracker_v1.py
+++ v1/leetcode_tracker_v1.py
@@ -4,3 +4,7 @@ import datetime
 easy = int(input("How many easy problems did you solve today? "))
 medium = int(input("How many medium problems did you solve today? "))
 hard = int(input("How many hard problems did you solve today? "))
+
+today = datetime.date.today()
+with open("progress.txt","a") as f:
+    f.write(f"{today},{easy},{medium},{hard}\n")
