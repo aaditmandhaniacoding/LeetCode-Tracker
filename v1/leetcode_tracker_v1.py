cffca6b3652a821adbedc713046073b9b047add4
Aadit Mandhania <aadit.mandhania.coding@gmail.com>
2025-10-25 13:26:52 -0500

    Add user input for daily easy, medium, and hard problem counts

--- v1/leetcode_tracker_v1.py
+++ v1/leetcode_tracker_v1.py
@@ -1,3 +1,6 @@
 import matplotlib.pyplot as plt
 import datetime
 
+easy = int(input("How many easy problems did you solve today? "))
+medium = int(input("How many medium problems did you solve today? "))
+hard = int(input("How many hard problems did you solve today? "))
